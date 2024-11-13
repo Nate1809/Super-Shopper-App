8f2a557eee3e490100d7387d35e53c65e8a1a14f
Nathan Guzman <monone18@me.com>
2024-11-12 18:20:21 -0600

    Add entitites and core data model

---
+++ CDShoppingItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CDShoppingItem+CoreDataClass.swift
+//  Super Shopper
+//
+//  Created by Nathan Guzman on 11/12/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CDShoppingItem)
+public class CDShoppingItem: NSManagedObject {
+
+}
---
+++ CDShoppingList+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CDShoppingList+CoreDataClass.swift
+//  Super Shopper
+//
+//  Created by Nathan Guzman on 11/12/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CDShoppingList)
+public class CDShoppingList: NSManagedObject {
+
+}
